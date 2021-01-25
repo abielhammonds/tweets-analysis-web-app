1face4ea69a410c5c257cc74a27d90aee22b6ab0
Ngoc <ngknocknguyen@gmail.com>
2021-01-24 21:09:39 -0600

    hello world to some sweet tweet

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    
+   # if request has args (?tweet=some_tweet)
+    if request.args:
+
+        # get the value of the arg
+        tweet = request.args.get('tweet')   
+        
+        return tweet
+
     return 'Hello World!'
 
 @app.route('/about')
@@ -15,6 +25,9 @@ def about():
     """
     return about_text
 
+
 if __name__=='__main__':
 
-    app.run(debug=True)
+     app.run(debug=True)
+
+
