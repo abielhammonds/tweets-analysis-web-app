3d95612b8a04a830da41edc2256f6398134df39e
BHuesmann <BrandiHuesmann@gmail.com>
2021-01-25 20:40:05 -0600

    Updated flask argument to test changes!

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     
-  return render_template('index.html')
+  return render_template('index.html', my_arg="I'm changing the text for testing purposes!")
 
 @app.route('/about')
 def about():
